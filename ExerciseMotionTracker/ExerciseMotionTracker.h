ed8841de4f0d2e58eb1464b731b4a70628cb96ab
Kurtis Gibson <kurtisgibsonmusic@gmail.com>
2016-11-18 10:57:54 -0600

    Initial Commit

---
+++ ExerciseMotionTracker/ExerciseMotionTracker.h
@@ -0,0 +1,19 @@
+//
+//  ExerciseMotionTracker.h
+//  ExerciseMotionTracker
+//
+//  Created by Kurtis Gibson on 2016-11-18.
+//  Copyright © 2016 Capstone G09. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ExerciseMotionTracker.
+FOUNDATION_EXPORT double ExerciseMotionTrackerVersionNumber;
+
+//! Project version string for ExerciseMotionTracker.
+FOUNDATION_EXPORT const unsigned char ExerciseMotionTrackerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExerciseMotionTracker/PublicHeader.h>
+
+
