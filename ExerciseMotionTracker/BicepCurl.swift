8f33afb976905177e7d0b5ff9572933291f0e221
Kurtis Gibson <kurtisgibsonmusic@gmail.com>
2017-03-25 15:17:54 -0500

    added initial angle function for bicepcurl

--- ExerciseMotionTracker/BicepCurl.swift
+++ ExerciseMotionTracker/BicepCurl.swift
@@ -10,8 +10,8 @@ import Foundation
 
 final public class BicepCurl: ExerciseMonitor
 {
-    let MAX_CURL_ANGLE: Float = (Float(120) * Float(M_PI)) / Float(180)
-    let MIN_CURL_ANGLE: Float = (Float(70) * Float(M_PI)) / Float(180)
+    let EXERCISE_RANGE: Float = 50
+    var MIN_CURL_ANGLE: Float = (Float(70) * Float(M_PI)) / Float(180)
         
     // returns percent complete of a bicep curl
     override public func getPercentComplete() -> Float? {
@@ -23,9 +23,23 @@ final public class BicepCurl: ExerciseMonitor
         
         let curlAngle = rightBicep?.getAngleBetweenJoints(otherJoint: rightForearm!)
         
-        percentComplete = (curlAngle! - MIN_CURL_ANGLE) /   (MAX_CURL_ANGLE - MIN_CURL_ANGLE)
+        percentComplete = (curlAngle! - MIN_CURL_ANGLE) / EXERCISE_RANGE
         
         return percentComplete
         
+    }
+    
+    // get initial curl angle for min angle
+    public func initAngle()
+    {
+        let rightBicep = skeleton.bodyJoints["rightBicep"]
+        let rightForearm = skeleton.bodyJoints["rightForearm"]
+        
+        let curlAngle = rightBicep?.getAngleBetweenJoints(otherJoint: rightForearm!)
+        
+        if (curlAngle != nil)
+        {
+            MIN_CURL_ANGLE = curlAngle!
+        }
     }
 }
