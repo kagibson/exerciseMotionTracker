f2a528d0d75c741a7636c9ecef1528a69060f386
Kurtis Gibson <kurtisgibsonmusic@gmail.com>
2017-04-07 13:37:20 -0500

    blablabla

--- ExerciseMotionTracker/OverheadPress.swift
+++ ExerciseMotionTracker/OverheadPress.swift
@@ -1,21 +1,69 @@
 //
-//  OverheadPress.swift
+//  BicepCurl.swift
 //  ExerciseMotionTracker
 //
-//  Created by Kurtis Gibson on 2017-04-02.
-//  Copyright © 2017 Capstone G09. All rights reserved.
+//  Created by Kurtis Gibson on 2016-11-27.
+//  Copyright © 2016 Capstone G09. All rights reserved.
 //
 
 import Foundation
 
 final public class OverheadPress: ExerciseMonitor
 {
-    override public func getPercentComplete() -> Int
-    {
-        return 0;
-    }
+    let EXERCISE_RANGE: Float = (Float(20) * Float(M_PI)) / Float(180)
+    var MAX_CURL_ANGLE: Float = (Float(70) * Float(M_PI)) / Float(180)
     
-    override public func initAngle() {
+    // returns percent complete of a bicep curl
+    override public func getPercentComplete() -> Int? {
+        
+        var percentComplete: Int
+        var percentCompleteFloat: Float
+        
+        let rightBicep = skeleton.bodyJoints["rightBicep"]
+        let rightForearm = skeleton.bodyJoints["rightForearm"]
+        
+        let curlAngle = rightBicep?.getAngleBetweenJoints(otherJoint: rightForearm!)
+        
+        if (curlAngle! > MAX_CURL_ANGLE)
+        {
+            percentComplete = 0;
+        }
+            
+        else if (curlAngle! < (MAX_CURL_ANGLE - EXERCISE_RANGE))
+        {
+            percentComplete = 100;
+        }
+            
+        else
+        {
+            percentCompleteFloat = (100.0 * (abs(curlAngle! - MAX_CURL_ANGLE) / EXERCISE_RANGE))
+            
+            if (!percentCompleteFloat.isNaN)
+            {
+                percentComplete = Int(percentCompleteFloat)
+            }
+            else
+            {
+                percentComplete = 0
+            }
+        }
+        
+        return percentComplete
         
     }
+    
+    // get initial curl angle for min angle
+    override public func initAngle()
+    {
+        let rightBicep = skeleton.bodyJoints["rightBicep"]
+        let rightForearm = skeleton.bodyJoints["rightForearm"]
+        
+        let curlAngle = rightBicep?.getAngleBetweenJoints(otherJoint: rightForearm!)
+        
+        if (curlAngle != nil)
+        {
+            MAX_CURL_ANGLE = curlAngle!
+        }
+    }
 }
+
